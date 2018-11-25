86565f87f81f68c92a61b726e0eb31e29dda80f2
Jero <tokidokitalkyou@gmail.com>
2018-11-26 06:46:27 +0800

    Interchangeable input formatting
    Successfully make the two QTextEdit as both input widgets. Text in the
    left input will convert into comma separated text. While text in the
    right input will convert into newline separated text.

--- main_ui.py
+++ main_ui.py
@@ -16,6 +16,7 @@ class Formi(QWidget):
 
         super().__init__(parent)
         self.clipboard = QApplication.clipboard()
+        self.formatted_text = ''
         self._widgets()
         self._properties()
         self._layouts()
@@ -23,34 +24,74 @@ class Formi(QWidget):
 
     def _widgets(self):
 
-        self.inputTextEdit = QPlainTextEdit()
-        self.outputTextEdit = QPlainTextEdit()
+        self.vertical_inputTextEdit = QPlainTextEdit()
+        self.horizontal_outputTextEdit = QPlainTextEdit()
 
     def _properties(self):
 
-        self.inputTextEdit.setPlaceholderText('Sample input text:\n\nbanana\napple\nkiwi\norange')
-        self.outputTextEdit.setPlaceholderText('Output text:\n\nbanana, apple, kiwi, orange')
+        self.vertical_inputTextEdit.setPlaceholderText('Sample input text:\n\nbanana\napple\nkiwi\norange')
+        self.vertical_inputTextEdit.setObjectName('vertical_inputTextEdit')
+        self.horizontal_outputTextEdit.setPlaceholderText('Output text:\n\nbanana, apple, kiwi, orange')
+        self.horizontal_outputTextEdit.setObjectName('horizontal_inputTextEdit')
         self.setWindowTitle(f'Formi {__version__} - text formatter For Humans')
         self.resize(471, 240)   # width, height
 
     def _layouts(self):
 
         hor = QHBoxLayout()
-        hor.addWidget(self.inputTextEdit)
-        hor.addWidget(self.outputTextEdit)
+        hor.addWidget(self.vertical_inputTextEdit)
+        hor.addWidget(self.horizontal_outputTextEdit)
         self.setLayout(hor)
 
     def _connections(self):
 
-        self.inputTextEdit.textChanged.connect(self.on_inputTextEdit_textChanged)
+        # self.vertical_inputTextEdit.textChanged.connect(self.on_vertical_inputTextEdit_textChanged)
+        # self.horizontal_outputTextEdit.textChanged.connect(self.on_horizontal_inputTextEdit_textChanged)
 
-    def on_inputTextEdit_textChanged(self):
+        # TEST: consolidate to one function
+        self.vertical_inputTextEdit.textChanged.connect(self.on_TextEdit_textChanged)
+        self.horizontal_outputTextEdit.textChanged.connect(self.on_TextEdit_textChanged)
 
-        input_text = self.inputTextEdit.toPlainText().strip()
-        formatted_text = formi.join_string(input_text)
+    def on_TextEdit_textChanged(self):
 
-        self.outputTextEdit.setPlainText(formatted_text)
-        self.clipboard.setText(formatted_text)
+        widget = self.sender()
+        print(widget.objectName())
+        if widget.objectName() == 'vertical_inputTextEdit':
+            self.horizontal_outputTextEdit.disconnect()
+            self.on_vertical_inputTextEdit_textChanged()
+            self.expand_text_horizontally()
+            self.horizontal_outputTextEdit.textChanged.connect(self.on_TextEdit_textChanged)
+            print(widget.objectName())
+        else:
+            self.vertical_inputTextEdit.disconnect()
+            self.on_horizontal_inputTextEdit_textChanged()
+            self.expand_text_vertically()
+            self.vertical_inputTextEdit.textChanged.connect(self.on_TextEdit_textChanged)
+            print(widget.objectName())
+
+    def on_vertical_inputTextEdit_textChanged(self):
+
+        # widget = self.sender()
+        # print(widget.objectName())
+        input_text = self.vertical_inputTextEdit.toPlainText().strip()
+        self.formatted_text = formi.join_string(input_text)
+
+    def expand_text_horizontally(self):
+
+        self.horizontal_outputTextEdit.setPlainText(self.formatted_text)
+        self.clipboard.setText(self.formatted_text)
+
+    def on_horizontal_inputTextEdit_textChanged(self):
+
+        # widget = self.sender()
+        # print(widget.objectName())
+        input_text = self.horizontal_outputTextEdit.toPlainText().strip()
+        self.formatted_text = formi.expand_string(input_text)
+
+    def expand_text_vertically(self):
+
+        self.vertical_inputTextEdit.setPlainText(self.formatted_text)
+        self.clipboard.setText(self.formatted_text)
 
     def keyPressEvent(self, event):
 
