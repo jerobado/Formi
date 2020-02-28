e550c0063520401c4880a1b2af9da378a90011f1
Jero Bado <tokidokitalkyou@gmail.com>
2020-02-28 11:29:01 +0800

    Added type hints for the two functions

--- src/core/formi.py
+++ src/core/formi.py
@@ -1,13 +1,14 @@
 """ Formi is a Python application that can format a copied rows/cells into comma separated text. """
 
 
-def join_string(text):
+# [] TODO: combine this two function into 1 function, probably called 'format_text(text, separator)
+def join_string(text: str) -> str:
     """ Return formatted text separated by comma ','. """
-    
+
     return text.replace('\n', ', ')
 
 
-def expand_string(text):
+def expand_string(text: str) -> str:
     """ Return formatted text separated by newline '\n'. """
 
     return '\n'.join(text.split()).replace(',', '')
