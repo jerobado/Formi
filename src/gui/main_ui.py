ba9adb2acaef784e4edc8611758c5e7759625b1f
Jero Bado <tokidokitalkyou@gmail.com>
2020-03-09 19:41:00 +0800

    Added Operations groupbox widget
    This will contain checkboxes, radio buttons or any widget that can be toggled to make certain changes to the input text such as removing duplicates, count text, etc.

--- src/gui/main_ui.py
+++ src/gui/main_ui.py
@@ -4,7 +4,11 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication,
                              QWidget,
                              QPlainTextEdit,
-                             QHBoxLayout)
+                             QVBoxLayout,
+                             QHBoxLayout,
+                             QGroupBox,
+                             QCheckBox,
+                             QRadioButton)
 from src.core import formi
 
 __version__ = '0.2.1'
@@ -26,6 +30,8 @@ class Formi(QWidget):
 
         self.vertical_inputTextEdit = QPlainTextEdit()
         self.horizontal_outputTextEdit = QPlainTextEdit()
+        self.operationsGroupBox = QGroupBox()
+        self.removeDuplicateCheckBox = QCheckBox()
 
     def _properties(self):
 
@@ -33,14 +39,23 @@ class Formi(QWidget):
         self.vertical_inputTextEdit.setObjectName('vertical_inputTextEdit')
         self.horizontal_outputTextEdit.setPlaceholderText('Output text:\n\nbanana, apple, kiwi, orange')
         self.horizontal_outputTextEdit.setObjectName('horizontal_inputTextEdit')
+        self.operationsGroupBox.setTitle('Operations')
+        self.removeDuplicateCheckBox.setText('Remove &duplicates')
         self.setWindowTitle(f'Formi {__version__} - text formatter For Humans')
         self.resize(471, 240)   # width, height
 
     def _layouts(self):
 
+        groupbox_layout = QVBoxLayout()
+        groupbox_layout.addWidget(self.removeDuplicateCheckBox)
+        groupbox_layout.addStretch()
+
+        self.operationsGroupBox.setLayout(groupbox_layout)
+
         hor = QHBoxLayout()
         hor.addWidget(self.vertical_inputTextEdit)
         hor.addWidget(self.horizontal_outputTextEdit)
+        hor.addWidget(self.operationsGroupBox)
         self.setLayout(hor)
 
     def _connections(self):
@@ -51,6 +66,8 @@ class Formi(QWidget):
         # TEST: consolidate to one function
         self.vertical_inputTextEdit.textChanged.connect(self.on_TextEdit_textChanged)
         self.horizontal_outputTextEdit.textChanged.connect(self.on_TextEdit_textChanged)
+
+        self.removeDuplicateCheckBox.clicked.connect(self.on_removeDuplicateCheckBox_clicked)
 
     def on_TextEdit_textChanged(self):
 
@@ -92,6 +109,10 @@ class Formi(QWidget):
 
         self.vertical_inputTextEdit.setPlainText(self.formatted_text)
         self.clipboard.setText(self.formatted_text)
+
+    def on_removeDuplicateCheckBox_clicked(self):
+
+        print('on_removeDuplicateCheckBox_clicked')
 
     def keyPressEvent(self, event):
 
